c5f65b95d0d212af1da6d68de3ed994d286920fb
Naufal <mnaufalpperdana@gmail.com>
2017-09-19 17:58:33 +0700

    jurnal naufal

---
+++ api/controllers/DetilPembelianController.js
@@ -0,0 +1,11 @@
+/**
+ * DetilPembelianController
+ *
+ * @description :: Server-side logic for managing detilpembelians
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
